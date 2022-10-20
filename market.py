bfea62a7248ebc61ec84481134b438361b898071
mhadani <mohammedhadani00@gmail.com>
2022-10-20 14:57:03 +0100

    Styling

--- market.py
+++ market.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
-
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f"<h1>This is the about page of {username}</h1>"
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
